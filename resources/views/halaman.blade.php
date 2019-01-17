ad221e09e629f91865636a0ff3fdf4bdb81bff1e
Ramdhan Eka Saputra <ramdhaneka90@gmail.com>
2019-01-17 15:16:30 +0700

    Start Project

--- resources/views/halaman.blade.php
+++ resources/views/halaman.blade.php
@@ -7,9 +7,6 @@
    <title>Halaman</title>
 </head>
 <body>
-   <h1>Urang ganteng pisan!</h1>
-   <h2>Ganteng keneh urg</h2>
-   <h3>sia dombis</h3>
-   <h4>Kumaha urang weh atuh... bacot!</h4>
+   
 </body>
 </html>
